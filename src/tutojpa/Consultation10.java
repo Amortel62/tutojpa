e0947b894ccf25f40c4e0ad095755acfc583c5d0
Administrateur <Administrateur@Win7n>
2017-11-06 12:06:09 +0100

    Apres Clone.

---
+++ src/tutojpa/Consultation10.java
@@ -0,0 +1,48 @@
+package tutojpa;
+
+import entites.Affectation;
+import entites.Projet;
+import entites.Salarie;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+
+
+public class Consultation10 {
+    
+    public static void main(String[] args){
+    
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("PU");
+    EntityManager         em = emf.createEntityManager();
+    
+         
+     Salarie sal = em.find(Salarie.class,103L);   
+     
+        System.out.println(sal.getNomsal());
+        System.out.println(sal.getSexe());
+        System.out.println(sal.getSalaire());
+        
+        System.out.println(sal.getLePole().getCodePole());
+        System.out.println(sal.getLePole().getNomPole());
+        
+      for(Affectation af : sal.getLesAffectations()){
+      
+          System.out.println(af.getDateaff());
+           System.out.println(af.isPilote());
+            System.out.println(af.getLeProjet().getDatedep());
+             System.out.println(af.getLeProjet().getDatefinp());
+              System.out.println(af.getLeProjet().getDescproj());
+               System.out.println(af.getLeProjet().getLeClient().getAdrcli());
+                System.out.println(af.getLeProjet().getLeClient().getNomcli());
+                  
+      
+      }
+  
+        
+        
+       
+    
+    
+}
+}
